7facdde088a3559dbeed5ec805ea74d6cb7ce203
Aloïs Micard <alois@micard.lu>
2022-07-05 17:07:54 +0200

    Compress PDF size

--- htdocs/core/class/commonobject.class.php
+++ htdocs/core/class/commonobject.class.php
@@ -5361,6 +5361,16 @@ abstract class CommonObject
 					// Success in building document. We build meta file.
 					dol_meta_create($this);
 
+					if ($conf->global->PDF_COMPRESSION) {
+						$cmd = '/usr/bin/gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile="%s" "%s"';
+						$cmd = sprintf($cmd, $obj->result['fullpath'].'.compressed', $obj->result['fullpath']);
+
+						exec($cmd);
+
+						unlink($obj->result['fullpath']);
+						rename($obj->result['fullpath'].'.compressed', $obj->result['fullpath']);
+					}
+
 					return 1;
 				} else {
 					$outputlangs->charset_output = $sav_charset_output;
