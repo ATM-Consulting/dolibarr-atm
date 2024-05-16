382df4e267f26ff5538290e7629a507436e16957
Jonathan lescaut <jonathan.lescaut@atm-consulting.fr>
2024-05-16 11:31:42 +0200

    retour PR

--- htdocs/core/modules/modProduct.class.php
+++ htdocs/core/modules/modProduct.class.php
@@ -967,7 +967,7 @@ class modProduct extends DolibarrModules
 				'pa.incdec' => "0",
 				'pa.rang' => "1");
 			$this->import_regex_array[$r] = array('pa.fk_product_pere' => 'rowid@'.MAIN_DB_PREFIX.'product', 'pa.fk_product_fils' => 'rowid@'.MAIN_DB_PREFIX.'product');
-			$this->import_updatekeys_array[$r] = array('pa.fk_product_pere' => 'ref parent', 'pa.fk_product_fils' => "ref enfant");
+			$this->import_updatekeys_array[$r] = array('pa.fk_product_pere' => 'import_ref_parent', 'pa.fk_product_fils' => "import_ref_child");
 		}
 	}
 
