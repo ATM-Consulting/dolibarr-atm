4690ca137692e4a4b075131927c6b7021bc6506d
jpb <jean-pascal.boudet@atm-consulting>
2024-06-24 11:52:49 +0200

    backport eb334e48f5d7bcff042669e65aabe2a9f7dc2597 from 19.0

--- htdocs/core/class/notify.class.php
+++ htdocs/core/class/notify.class.php
@@ -213,7 +213,7 @@ class Notify
 
 		if (!$error) {
 			if ($socid >= 0 && in_array('thirdparty', $scope)) {
-				$sql = "SELECT a.code, c.email, c.rowid";
+				$sql = "SELECT a.code, c.email, c.rowid, , c.statut as status";
 				$sql .= " FROM ".$this->db->prefix()."notify_def as n,";
 				$sql .= " ".$this->db->prefix()."socpeople as c,";
 				$sql .= " ".$this->db->prefix()."c_action_trigger as a,";
@@ -235,7 +235,7 @@ class Notify
 					$i = 0;
 					while ($i < $num) {
 						$obj = $this->db->fetch_object($resql);
-						if ($obj) {
+						if ($obj && $obj->status ==  1) {
 							$newval2 = trim($obj->email);
 							$isvalid = isValidEmail($newval2);
 							if (empty($resarray[$newval2])) {
